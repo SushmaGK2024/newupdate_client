58a2e5a566e12a249ceed28bcdde5e4e554f4041
SushmaGK2024 <sushmagkulkarni23@gmail.com>
2024-05-11 21:16:25 +0530

    reset

--- src/store/index.js
+++
@@ -1,7 +0,0 @@
-// src/store/index.js
-import { createStore } from 'redux';
-import rootReducer from './reducers/rootReducer'; // Import your root reducer
-
-const store = createStore(rootReducer);
-
-export default store;
--- src/store/reducers/rootReducer.js
+++
@@ -1,9 +0,0 @@
-// src/reducers/rootReducer.js
-import { combineReducers } from 'redux';
-// Import your individual reducers here
-
-const rootReducer = combineReducers({
-  // Add your individual reducers here
-});
-
-export default rootReducer;
